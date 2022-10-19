2ad3f8c13f685c0f5894658e98c289d13df0e51b
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-10-19 16:54:50 +0700

    Add Class Program

--- Activity2_20210140061_A/Program.cs
+++ Activity2_20210140061_A/Program.cs
@@ -2,5 +2,14 @@
 
 namespace QuickSort
 {
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comprasion
+        private int mov_count = 0; // number of data movements
 
+        private int n;
+
+    }
 }
